81f902e3f7f543e0123e06cde2657a00b887f567
rsha1 <62945933+rsha1@users.noreply.github.com>
2025-04-10 13:23:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ECOReport AI Chat",
+  description: "ECOReport's AI chat service.",
   appleWebApp: {
-    title: "NextChat",
+    title: "ECOReport AI Chat chat",
     statusBarStyle: "default",
   },
 };
